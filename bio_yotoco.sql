8271815ea504935e3602f8b2bab44970beccb5e1
BirdCode888 <birdcode.cali@gmail.com>
2026-02-22 20:54:26 -0500

    Integracion de indicadores de remanente de ecosistema natural y area a reforestar en capa de predios

--- bio_yotoco.sql
+++ bio_yotoco.sql
@@ -1,8 +1,15 @@
--- Calculo de área a reforestar por predio
+-- ===========================================================================================================
+
+-- PROYECTO DE REFORESTACIÓN EN PREDIOS DE RESERVA FORESTAL GUADUALITOS - MUNICIPIO DE YOTOCO VALLE DEL CAUCA
+
+-- ===========================================================================================================
+
+---- Calculo de área a reforestar por predio
 
 SELECT current_schema();
 SET search_path TO guadualitos, public;
 
+CREATE VIEW area_a_reforestar AS
 SELECT
 	a.codigo AS cedula_catastral, 
 	ROUND(a.area_ha::numeric,2) AS area_has,
@@ -17,6 +24,37 @@ GROUP BY
 	a.area_ha
 ORDER BY
 	area_a_reforestar DESC; 
+
+SELECT * FROM area_a_reforestar;
+
+-------------------------------------------------------------------------------------------------------------------
+
+---- Integracion de indicadores de remanente de ecosistema natural y area a reforestar en capa de predios
+
+------ Creacion de campos correspondientes en la tabla de predios (catastro_bio_yotoco)
+
+ALTER TABLE 
+	catastro_bio_yotoco 
+ADD COLUMN 
+	remanente_eco_natural double precision,
+ADD COLUMN
+	area_a_reforestar double precision;
+
+------ Integracion de indicadores
+
+UPDATE 
+	catastro_bio_yotoco  
+SET
+	remanente_eco_natural = a.remanente_ecosistema_natural,
+	area_a_reforestar = a.area_a_reforestar
+FROM
+	area_a_reforestar a
+WHERE
+	catastro_bio_yotoco.codigo = a.cedula_catastral;
+
+------------------------------------------------------------------------------------------------------------------
+	
+
 
 
 	
