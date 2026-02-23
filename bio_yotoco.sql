a9c07cb644dcccbdc829b406e19c1d42d20dcb2a
BirdCode888 <birdcode.cali@gmail.com>
2026-02-22 20:15:31 -0500

    Ajustes a codigo y tabla - Calculo de area a reforestar por predio

--- bio_yotoco.sql
+++ bio_yotoco.sql
@@ -4,16 +4,16 @@ SELECT current_schema();
 SET search_path TO guadualitos, public;
 
 SELECT
-	a.codigo, 
-	a.area_ha,
-	(SUM(ST_Area(ST_Intersection(a.geom, b.geom)))/10000.0) AS remanente_ecosistema_natural,
-	a.area_ha - (SUM(ST_Area(ST_Intersection(a.geom, b.geom)))/10000.0) AS area_a_reforestar
+	a.codigo AS cedula_catastral, 
+	ROUND(a.area_ha::numeric,2) AS area_has,
+	ROUND((SUM(ST_Area(ST_Intersection(a.geom, b.geom)))/10000.0)::numeric,2) AS remanente_ecosistema_natural,
+	ROUND(a.area_ha - (SUM(ST_Area(ST_Intersection(a.geom, b.geom)))/10000.0)::numeric,2) AS area_a_reforestar
 FROM
 	catastro_bio_yotoco a
 JOIN
 	fragmentacion_guadualitos b ON ST_Intersects(a.geom, b.geom)
 GROUP BY
-	a.codigo,
+	cedula_catastral,
 	a.area_ha
 ORDER BY
 	area_a_reforestar DESC; 
